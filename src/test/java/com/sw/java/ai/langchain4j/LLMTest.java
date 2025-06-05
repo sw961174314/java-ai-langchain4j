6f4035b265ac2f93909c051d0476f4123762a1d4
sw <961174314@qq.com>
2025-06-05 09:08:26 +0800

    SpringBoot整合LangChain4j

---
+++ src/test/java/com/sw/java/ai/langchain4j/LLMTest.java
@@ -0,0 +1,34 @@
+package com.sw.java.ai.langchain4j;
+
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class LLMTest {
+
+    @Autowired
+    private OpenAiChatModel openAiChatModel;
+
+    @Test
+    public void testGptDemo() {
+        //初始化模型
+        OpenAiChatModel model = OpenAiChatModel.builder()
+        //LangChain4j提供的代理服务器，该代理服务器会将演示密钥替换成真实密钥， 再将请求转发给OpenAI API
+            .baseUrl("http://langchain4j.dev/demo/openai/v1") //设置模型api地址（如果apiKey="demo"，则可省略baseUrl的配置）
+            .apiKey("demo") //设置模型apiKey
+            .modelName("gpt-4o-mini") //设置模型名称
+            .build();
+        //向模型提问
+        String answer = model.chat("你好");
+        //输出结果
+        System.out.println(answer);
+    }
+
+    @Test
+    public void testSpringBoot() {
+        String answer = openAiChatModel.chat("我是谁");
+        System.out.println(answer);
+    }
+}
