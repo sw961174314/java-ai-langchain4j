036f1a4eb15c53242bbc87ca66ed3eb207d3c290
sw <961174314@qq.com>
2025-06-05 08:18:39 +0800

    项目初始化

---
+++ src/main/java/com/sw/aiApp.java
@@ -0,0 +1,11 @@
+package com.sw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class aiApp {
+    public static void main(String[] args) {
+        SpringApplication.run(aiApp.class, args);
+    }
+}
