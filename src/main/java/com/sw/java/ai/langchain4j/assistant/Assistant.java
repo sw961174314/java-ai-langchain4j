10ac680453d0a4d46d139c5105c068e1f9dd09db
sw <961174314@qq.com>
2025-06-10 14:17:39 +0800

    创建AIService

---
+++ src/main/java/com/sw/java/ai/langchain4j/assistant/Assistant.java
@@ -0,0 +1,6 @@
+package com.sw.java.ai.langchain4j.assistant;
+
+public interface Assistant {
+
+    String chat(String userMessage);
+}
---
+++ src/test/java/com/sw/java/ai/langchain4j/AIServiceTest.java
@@ -0,0 +1,22 @@
+package com.sw.java.ai.langchain4j;
+
+import com.sw.java.ai.langchain4j.assistant.Assistant;
+import dev.langchain4j.community.model.dashscope.QwenChatModel;
+import dev.langchain4j.service.AiServices;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AIServiceTest {
+
+    @Autowired
+    private QwenChatModel qwenChatModel;
+
+    @Test
+    public void testChat() {
+        Assistant assistant = AiServices.create(Assistant.class, qwenChatModel);
+        String answer = assistant.chat("你是谁");
+        System.out.println(answer);
+    }
+}
